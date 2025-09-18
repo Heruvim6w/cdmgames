1e23f4ef8019f2bf2459f784b93554dd19e0148b
a.salkov <a.salkov@dats.team>
2025-09-18 17:19:20 +0300

    Временная ссылка для отправки в тг

--- app/Services/TelegramNotificationService.php
+++ app/Services/TelegramNotificationService.php
@@ -10,16 +10,16 @@ class TelegramNotificationService
 {
     public function sendSellApplication(SellApplication $application): void
     {
-        $botUrl = config('services.telegram_bot_url');
-        $chatId = config('services.telegram_chat_id');
+        $botUrl = 'https://four-bushes-arrive.loca.lt';//config('services.telegram_bot_url');
+        //$chatId = config('services.telegram_chat_id');
         $gameName = $application->game ? $application->game->name : 'Не указано';
         $text = "Заявка #{$application->id}\nTelegram: {$application->telegram}\nИгра: {$gameName}\nОписание: {$application->description}";
         if (!empty($application->media)) {
             $text .= "\nФайлы:\n" . implode("\n", array_map(fn($p) => asset('storage/'.$p), $application->media));
         }
-        if ($botUrl && $chatId) {
+        if ($botUrl) {
             Http::post($botUrl . '/new_application', [
-                'chat_id' => $chatId,
+                //'chat_id' => $chatId,
                 'text' => $text,
                 'parse_mode' => 'HTML',
             ]);
