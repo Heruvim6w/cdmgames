b4723c648e67d3fdfaa8492ef0e5bd547802a55f
a.salkov <a.salkov@dats.team>
2025-09-24 14:33:52 +0300

    Add game id to sell an application message

--- app/Services/TelegramNotificationService.php
+++ app/Services/TelegramNotificationService.php
@@ -10,8 +10,9 @@ class TelegramNotificationService
     public function sendSellApplication(SellApplication $application): void
     {
         $botUrl = config('services.telegram_bot_url');
+        $gameId = $application->game_id ?? null;
         $gameName = $application->game ? str_replace('Продать аккаунт ', '', $application->game->name) : 'Не указано';
-        $text = "Заявка #{$application->id}\nTelegram: {$application->telegram}\nИгра: {$gameName}\nОписание: {$application->description}";
+        $text = "Заявка #{$application->id}\nTelegram: {$application->telegram}\nИгра: {$gameName}\nID игры: {$gameId}\nОписание: {$application->description}";
         if (!empty($application->media)) {
             $text .= "\nФайлы:\n" . implode("\n", array_map(fn($p) => asset('storage/'.$p), $application->media));
         }
